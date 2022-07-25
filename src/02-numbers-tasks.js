8e445bb679bd3aefac691783d97200dbf7b7b0f6
SPECTRA995 <maxmillerasce@gmail.com>
2022-07-26 00:28:08 +0300

    fix: Error: Unexpected token: operator (<) try5

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -155,8 +155,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const coss = (x1 * x2 + y1 * y2) / (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2));
-  return Math.acos(coss);
+  const cos = (x1 * x2 + y1 * y2) / (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2));
+  return Math.acos(cos);
 }
 
 /**
